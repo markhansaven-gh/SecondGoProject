15509f9ac00b98c8e2aa9808e700d1874d5b8eb4
Mark Hans Aven <markhansaven@gmail.com>
2025-07-16 22:11:25 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	println("Hello this is testing what I've already memorized for the Go progrmaming language!")
+
+	var x int = 5
+	var y int = 6
+	var z int = 7
+	fmt.Printf("Coords are: %d %d %d", x, y, z)
+}
